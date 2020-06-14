e48df7f7322e2350474a7244c9599375dd184036
ReyDonovan <gennady.zamalaev@gmail.com>
2020-06-14 18:30:52 +0300

    DB: release LMDB 548.01 https://github.com/ReyDonovan/LegacyMoP/releases/tag/LMDB

--- sql/updates/world/2020_04_01_03_world.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `instance_template` WHERE `map` = 1134;
-INSERT INTO `instance_template` (`map`, `parent`, `script`, `allowMount`) VALUES ('1134', '0', '', '0');
--- sql/updates/world/2020_04_01_08_world.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` IN (97993, 22842);
-INSERT INTO `spell_script_names` VALUE 
-(97993, 'spell_dru_stampeding_roar'),
-(22842, 'spell_dru_frenzied_regeneration');
